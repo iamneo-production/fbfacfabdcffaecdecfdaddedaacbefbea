5d9309f5694ee4bc47b9fc4ea95d8d41150e85e7
Iamneo <techadmin@iamneo.ai>
2023-09-12 22:48:13 +0000

    Backup Commit

---
+++ create_custom_permissions.sh
@@ -0,0 +1,14 @@
+directory="/home/coder"
+folder_name="new_folder"
+
+mkdir "$directory/$folder_name"
+chmod 644 "$directory/$folder_name"
+
+file_name="new_file.txt"
+file_content="This is a new file with custom permissions."
+
+echo "$file_content" > "$directory/$folder_name/$file_name"
+
+chmod 744 "$directory/$folder_name/$file_name"
+
+echo "Created folder '$folder_name' and file '$file_name' in '$directory' with custom permissions."
