7bf72639745b3111804bb6efa20c8f75535848b3
Iamneo <techadmin@iamneo.ai>
2023-09-12 22:38:13 +0000

    Backup Commit

--- create_and _write_file.sh
+++
@@ -1,7 +0,0 @@
-file_path="/home/code/project/workspace/create_and_write_file.sh"
-
-content="Hello, this is some text content written to the file."
-
-echo "$content" > "$file_path"
-
-echo "File created and text content written successfully."
