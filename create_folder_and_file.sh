cdd2a3af2d7a01e28c72ee54f349d0f25dc010df
Iamneo <techadmin@iamneo.ai>
2023-09-12 22:08:13 +0000

    Backup Commit

---
+++ create_folder_and_file.sh
@@ -0,0 +1,9 @@
+directory="/home/coder"
+
+mkdir "$directory/new_folder"
+
+touch "$directory/new_folder/new_file.txt"
+
+echo "This is some content." > "$directory/new_folder/new_file.txt"
+
+echo "Folder and file created successfully."
