cd8ff873b910b2c35527ee6409c3eb1cfd0e02dd
Iamneo <techadmin@iamneo.ai>
2023-09-13 00:39:13 +0000

    Backup Commit

--- create_file.sh
+++
@@ -1,7 +0,0 @@
-file_name="new_file.txt"
-file_content="This is some text content that I'm writing to a new file."
-
-echo "$file_content" > "$file_name"
-
-echo "Created file '$file_name' with content:"
-cat "$file_name"
--- create_folder_and_file.sh
+++
@@ -1,9 +0,0 @@
-directory="/home/coder"
-
-mkdir "$directory/new_folder"
-
-touch "$directory/new_folder/new_file.txt"
-
-echo "This is some content." > "$directory/new_folder/new_file.txt"
-
-echo "Folder and file created successfully."
